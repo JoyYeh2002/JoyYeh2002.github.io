5bdd4d3ed5eb1f5f6776ce487d46587e9e809883
AstroJays <66350727+JoyYeh2002@users.noreply.github.com>
2022-08-03 16:39:47 -0400

    Counter would count every time we enter but whatever

--- exercises/animated_tally/script.js
+++ exercises/animated_tally/script.js
@@ -1,22 +1,61 @@
 let valueDisplays = document.querySelectorAll(".num");
-let interval = 3000;
+let interval = 1000;
+var delayInMilliseconds = 500; //1 second
 
-valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function () {
+const header = document.querySelector("stats-wrapper");
+const sectionOne = document.querySelector(".stats-wrapper");
 
-        if (endValue == 4) {
-            startValue += 1;
-        }
-        else {
-            startValue += 4;
-        }
-        valueDisplay.textContent = startValue;
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
 
-        if (startValue == endValue) {
-            clearInterval(counter);
+const sectionOneObserver = new IntersectionObserver(function (
+    entries,
+    sectionOneObserver
+) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            setTimeout(function () {
+                valueDisplays.forEach((valueDisplay) => {
+                    let startValue = 0;
+                    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+                    let duration = Math.floor(interval / endValue);
+                    let counter = setInterval(function () {
+
+                        if (endValue == 4) {
+                            startValue += 1;
+                        }
+                        else {
+                            startValue += 4;
+                        }
+                        valueDisplay.textContent = startValue;
+
+                        if (startValue == endValue) {
+                            clearInterval(counter);
+                        }
+                    }, duration);
+                });
+            }, delayInMilliseconds);
         }
-    }, duration);
-});
+
+    });
+},
+    sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+
